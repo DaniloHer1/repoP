a4d381d799541d67d9a16b57507615ce8a6f329c
Daniel <clasec2comercio@gmail.com>
2025-02-19 13:55:37 +0100

    Commit desde NETB

---
+++ src/main/java/com/mycompany/gh/PRueba2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.gh;
+
+/**
+ *
+ * @author Diurno
+ */
+public class PRueba2 {
+    public static void main(String[] args) {
+        
+    }
+    
+}
